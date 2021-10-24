01eb3d76a96a761967b7c6c9393a20878f2f6d8e
kochan0805 <kanata.y.psyche@gmail.com>
2021-10-24 11:50:56 +0900

    フォルダ名変更

--- Machine Learning_training.py
+++ Machine Learning_training.py
@@ -123,3 +123,26 @@ plt.ylim(-3, 3)
 plt.xlabel("Feature")
 plt.ylabel("Target")
 # %%
+from sklearn.datasets import load_breast_cancer
+cancer = load_breast_cancer()
+print("cancer.keys(): \n{}".format(cancer.keys()))
+# %%
+print("Shape of cancer data: {}".format(cancer.data.shape))
+# %%
+print("Sample counts per class:\n{}".format(
+    {n: v for n, v in zip(cancer.target_names, np.bincount(cancer.target))}
+))
+# %%
+print("Feature names:\n{}".format(cancer.feature_names))
+# %%
+from sklearn.datasets import load_boston
+boston = load_boston()
+print("Data shape: \n{}".format(boston.data.shape))
+# %%
+X, y = mglearn.datasets.load_extended_boston()
+print("X.shape: {}".format(X.shape))
+# %%
+mglearn.plots.plot_knn_classification(n_neighbors=1)
+# %%
+mglearn.plots.plot_knn_classification(n_neighbors=3)
+# %%
